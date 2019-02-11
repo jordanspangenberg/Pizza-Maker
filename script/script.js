38decf55bd909d9156933a587c405a7418ccb7f6
Jordan Spangenberg <jordan.spang@gmail.com>
2019-02-10 16:01:12 -0800

    Create table

--- script/script.js
+++ script/script.js
@@ -1,22 +1,30 @@
-function Pizza(size, meats, cheese, crust, sauce, veggies) {
-  this.size = size
-  this.meats = meats
-  this.cheese = cheese
-  this.crust = crust
-  this.sauce = sauce
-  this.veggies = veggies
-}
-
 function makePizza() {
   var fieldNames = ['size', 'crust', 'cheese', 'sauce', 'meats', 'veggies']
+  var myPizza = Object.create(null)
   for (var i = 0; i < fieldNames.length; i++) {
     var fields = document.getElementsByName(fieldNames[i])
-    var Pizzas = []
+    var pizzaToppings = []
     for (var j = 0; j < fields.length; j++) {
       if (fields[j].checked) {
-        console.log(fields[j].value)
-        Pizzas.push(fields[j].value)
+        pizzaToppings.push(new Object('{' + fieldNames[i] + ': ' + fields[j].value + ', price: ' + fields[j].dataset.price + '};'))
       }
+    }
+    myPizza[fieldNames[i]] = pizzaToppings
+  }
+  return myPizza
+}
+
+
+
+function tableCreate() {
+  var varPizza = makePizza()
+  var fieldNames = ['size', 'crust', 'cheese', 'sauce', 'meats', 'veggies']
+  var table = $('#receipt')
+  for (var field of fieldNames) {
+    console.log(varPizza[field])
+    for (var topping of varPizza[field]) {
+      var row = $('<tr/>').addClass('pizza').html('<td>' + topping[field] + '</td>' + '<td>' + topping[price] + '</td>')
+      table.append(row)
     }
   }
 }
